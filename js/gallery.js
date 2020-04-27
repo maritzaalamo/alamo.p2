25145ce0e2ad9d58c83ce6a595a65eaf7926e603
maritzaalamo <39917899+maritzaalamo@users.noreply.github.com>
2020-04-27 19:18:15 -0400

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,15 @@
 // requestAnim shim layer by Paul Irish
     window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       || 
-              window.webkitRequestAnimationFrame || 
-              window.mozRequestAnimationFrame    || 
-              window.oRequestAnimationFrame      || 
-              window.msRequestAnimationFrame     || 
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              window.oRequestAnimationFrame      ||
+              window.msRequestAnimationFrame     ||
               function(/* function */ callback, /* DOMElement */ element){
                 window.setTimeout(callback, 1000 / 60);
               };
     })();
-  
+
 
 // example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
 
@@ -35,7 +35,7 @@ function animate() {
 function swapPhoto() {
 	//Add code here to access the #slideShow element.
 	//Access the img element and replace its source
-	//with a new image from your images array which is loaded 
+	//with a new image from your images array which is loaded
 	//from the JSON string
 	console.log('swap photo');
 }
@@ -43,8 +43,18 @@ function swapPhoto() {
 // Counter for the mImages array
 var mCurrentIndex = 0;
 
-// XMLHttpRequest variable
+function fetchJSON() {
 var mRequest = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+  if (this.readyState == 4 && this.status == 200) {
+    var images = this.responseText;
+    var imagesObj = JSON.parse(images);
+  }
+  }
+  xhttp.open("GET", "images.json",true);
+  xhttp.send();
+}
+
 
 // Array holding GalleryImage objects (see below).
 var mImages = [];
@@ -54,7 +64,7 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
+var mUrl = 'images.json';
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
@@ -67,22 +77,27 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 }
 
 $(document).ready( function() {
-	
+
 	// This initially hides the photos' metadata information
 	$('.details').eq(0).hide();
-	
+
 });
 
 window.addEventListener('load', function() {
-	
+
 	console.log('window loaded');
 
 }, false);
 
-function GalleryImage() {
+function GalleryImage(location, description, date) {
 	//implement me as an object to hold the following data about an image:
 	//1. location where photo was taken
+  var location;
 	//2. description of photo
+  var description;
 	//3. the date when the photo was taken
+  var date;
 	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+  var img;
+
 }
